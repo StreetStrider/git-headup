1a62acfc776f5c25165adc0c2e2496659b4ed1b7
StreetStrider <teamfortresslife@gmail.com>
2015-11-30 02:17:26 +0300

    git-config unwrap std.

--- run/lib/git/git-config.js
+++ run/lib/git/git-config.js
@@ -1,7 +1,11 @@
 
 var git = require('./git')
 
+var R = require('ramda')
+var unwrap = R.pipe(R.nth(0), R.trim)
+
 module.exports = function config (key)
 {
 	return git('config', '--get', key)
+	.then(unwrap)
 }
