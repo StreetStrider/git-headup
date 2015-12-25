6d93e9bd4ec367f3f2dcb9c29c5851a807856836
StreetStrider <teamfortresslife@gmail.com>
2015-12-26 00:36:10 +0300

    git: branch detached head.

--- run/lib/git/branch.js
+++ run/lib/git/branch.js
@@ -1,7 +1,10 @@
 
 var git = require('./git')
 
+var value = require('ramda').always
+var Null = value(null)
+
 module.exports = function branch ()
 {
-	return git('symbolic-ref --short HEAD')
+	return git('symbolic-ref --short HEAD').catch(Null)
 }
