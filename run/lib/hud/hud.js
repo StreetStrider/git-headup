e09782148b14d1ad5ed1ab500a93bb8d6e6684f8
StreetStrider <teamfortresslife@gmail.com>
2015-12-25 17:11:42 +0300

    write -> hud.

--- run/lib/hud/hud.js
+++ run/lib/hud/hud.js
@@ -5,3 +5,13 @@ hud.pipe  = '|'
 hud.space = ' '
 hud.nl    = '\n'
 hud.ell   = '…'
+
+var write = hud.write = function (string)
+{
+	process.stdout.write(string, 'utf-8')
+}
+
+hud.writeln = function (string)
+{
+	write(string + hud.nl)
+}
