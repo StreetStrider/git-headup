1443112eba3df9eb94f94226c85281457931eff7
StreetStrider <teamfortresslife@gmail.com>
2015-12-26 19:44:10 +0300

    git: fix git trim brokes status.

--- run/lib/git/git.js
+++ run/lib/git/git.js
@@ -1,9 +1,10 @@
 
 var run = require('command-promise')
+var trimRight = require('ramda').invoker(0, 'trimRight')
 
 module.exports = function git ()
 {
 	return run('git', arguments)
 	.then(run.util.stdout)
-	.then(run.util.trim)
+	.then(trimRight)
 }
