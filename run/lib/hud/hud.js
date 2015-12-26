2117d7283771be6ecf4802c408ab9219523bb02b
StreetStrider <teamfortresslife@gmail.com>
2015-12-27 01:11:12 +0300

    hud.writeseq.

--- run/lib/hud/hud.js
+++ run/lib/hud/hud.js
@@ -22,3 +22,25 @@ hud.writeln = function (string)
 {
 	write(string + hud.nl)
 }
+
+
+var maybe = require('../maybe')
+
+hud.writeseq = function writeseq (seq, promise)
+{
+	promise || (promise = Promise.resolve())
+
+	if (seq.length)
+	{
+		var head = seq[0]
+		    seq  = seq.slice(1)
+
+		promise = promise.then(head).then(maybe(hud.writeln))
+
+		return writeseq(seq, promise)
+	}
+	else
+	{
+		return promise.catch(console.error)
+	}
+}
