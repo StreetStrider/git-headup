69f68ae72ce82be1c2b8a8a3a4669186bc7f2445
StreetStrider <teamfortresslife@gmail.com>
2015-12-29 00:52:19 +0300

    hud.ellipsed.

--- run/lib/hud/hud.js
+++ run/lib/hud/hud.js
@@ -13,6 +13,22 @@ hud.brkt = function (string)
 }
 
 
+var len = require('cli-color').getStrippedLength
+var slice = require('cli-color').slice
+var curry = require('ramda').curry
+
+hud.ellipsed = curry(function (width, string)
+{
+	if (len(string) > width)
+	{
+		string = slice(string, 0, width - 2)
+		string = string + hud.ell + hud.space
+	}
+
+	return string
+})
+
+
 var write = hud.write = function (string)
 {
 	process.stdout.write(string, 'utf-8')
