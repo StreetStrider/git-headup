7bd4d46b29d8b5bed9d60f64a9e5a26cd23119a3
StreetStrider <teamfortresslife@gmail.com>
2016-01-05 00:15:21 +0300

    hud/npm: capture non-package.json-like stuff.

--- run/lib/hud/npm.js
+++ run/lib/hud/npm.js
@@ -17,23 +17,45 @@ module.exports = function npm ()
 {
 	try
 	{
-		var path = npmroot(process.cwd())
+		var manifest = capture()
 	}
 	catch (e)
 	{
 		return
 	}
 	{
-		return output(path)
+		return output(manifest)
 	}
 }
 
-function output (path)
+function capture (path)
 {
-	var line = bold(hud.pipe) + hud.space + bold('npm') + hud.space + hud.bull + hud.space
+	try
+	{
+		var path = npmroot(process.cwd())
+	}
+	catch (e) { throw e }
 
 	path = join(path, 'package.json')
-	var manifest = fs.readJSON(path)
+
+	try
+	{
+		var manifest = fs.readJSON(path)
+	}
+	catch (e) { throw e }
+
+	if (! manifest.name)
+	{
+		throw new TypeError
+	}
+
+	return manifest
+}
+
+
+function output (manifest)
+{
+	var line = bold(hud.pipe) + hud.space + bold('npm') + hud.space + hud.bull + hud.space
 
 	line = line + style__name(manifest.name)
 
